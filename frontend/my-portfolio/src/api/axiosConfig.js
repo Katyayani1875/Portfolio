1d55cf5ce63af4672e4ac4ce04e6471fe0262e5d
Gurusewak <gurusewaksingh122@gmail.com>
2025-05-23 12:23:23 +0530

    Provided backend

--- frontend/my-portfolio/src/api/axiosConfig.js
+++ frontend/my-portfolio/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000', // or your backend URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://portfolio1-ya3r.onrender.com', // or your backend URL
   timeout: 5000,
 });
 
